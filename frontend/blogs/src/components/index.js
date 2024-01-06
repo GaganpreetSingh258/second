c4c7e621620143f7c81ef397cd9340f1b1e3b789
GaganpreetSingh258 <gaganpreetsingh2277@gmail.com>
2024-01-06 16:25:18 +0530

    Initial commit

---
+++ frontend/blogs/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./header/Header";
+import Footer from "./Footer";
+import Container from "./container/container";
+import Logo from "./Logo";
+import Logoutbtn from "./header/Logoutbtn";
+
+
+export {Header,Footer,Container,Logo,Logoutbtn}
